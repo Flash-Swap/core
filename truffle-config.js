40790b7b8e8cdf16f0fb16a62fdff2de66f5cb50
Flash-Swap <flashswap.xyz@gmail.com>
2021-03-08 17:20:26 +0100

    truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -82,7 +82,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "^0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.5.16",    // Fetch exact version from solc-bin (default: truffle's version)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
        optimizer: {
          enabled: true,
